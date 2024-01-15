cba242d4add5b0935f3bbbc1223d4b21e38ad7f0
Noopur Bhatt <noopursbhatt@gmail.com>
2024-01-14 19:30:32 -0500

    support specifying prompts to test

--- run_experiments.py
+++ run_experiments.py
@@ -6,19 +6,27 @@ import argparse
 import os
 from generate_test_cases import run_generate_new_tests
 import shutil
+import json
 
 # Argument parser setup
 parser = argparse.ArgumentParser()
 parser.add_argument("-m", "--model", help="OpenAI GPT API model name")
-parser.add_argument("-p", "--num_prompts", help="Number of prompts to test")
+parser.add_argument("-p", "--num_prompts", help="Number of prompts to test or list of prompt numbers")
 parser.add_argument("-n", "--num_runs", help="Number of runs for prompt")
 parser.add_argument("-t", "--test_type", help="Type of test to run: new or original")
 parser.add_argument("-d", "--delta_method", help="Method for generating delta: permutations or combinations")
 args = parser.parse_args()
 
+# Function to parse the num_prompts argument
+def parse_num_prompts(arg):
+    if arg.startswith('[') and arg.endswith(']'):
+        return json.loads(arg)
+    else:
+        return int(arg)
+
 args_dict = {
     'model': args.model,
-    'num_prompts': int(args.num_prompts),
+    'num_prompts': parse_num_prompts(args.num_prompts),
     'num_runs': int(args.num_runs),
     'test_type': args.test_type,
     'delta_method': args.delta_method
@@ -41,14 +49,17 @@ else:
     print('New test cases already exist. Skipping generation...')
     df = pd.read_json('mbpp_new_test_cases.jsonl', lines=True)
 
-# pick random prompt numbers to test
-counter = 0
-prompt_numbers = []
-while counter < args_dict['num_prompts']:
-    prompt_number = random.randint(0, len(df) - 1)
-    if prompt_number not in prompt_numbers and df['new_test_list'][prompt_number] != 'Error generating new test cases':
-        prompt_numbers.append(prompt_number)
-        counter += 1
+# Determine prompt numbers to test
+if isinstance(args_dict['num_prompts'], list):
+    # Use the provided list of prompt numbers
+    prompt_numbers = args_dict['num_prompts']
+else:
+    # Pick random prompt numbers to test
+    prompt_numbers = []
+    while len(prompt_numbers) < args_dict['num_prompts']:
+        prompt_number = random.randint(0, len(df) - 1)
+        if prompt_number not in prompt_numbers and df['new_test_list'][prompt_number] != 'Error generating new test cases':
+            prompt_numbers.append(prompt_number)
 
 print('Prompt numbers to test:', prompt_numbers)
 
