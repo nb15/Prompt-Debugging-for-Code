2cb85ffd15a9051fb1e1dbc757c38ce134902015
Noopur Bhatt <noopursbhatt@gmail.com>
2024-01-16 13:15:46 -0500

    generate basic global report

--- analyze_results.py
+++ analyze_results.py
@@ -1,31 +1,43 @@
 import os
 import pandas as pd
 from generate_prompt_report import generate_pdf_report
+from generate_global_report import generate_global_pdf_report
 
-def process_csv_in_folder(folder_path, perform_individual_analysis):
+def process_and_aggregate_csv_files(base_path, perform_individual_analysis):
     """
-    Process each CSV file in a folder and generate a PDF report.
+    Process each CSV file in each folder for individual report generation and aggregate data for global report.
 
-    :param folder_path: Path of the folder containing CSV files.
+    :param base_path: The base directory containing the prompt directories.
     :param perform_individual_analysis: Boolean to decide whether to perform individual analysis.
+    :return: A DataFrame containing aggregated data from all CSV files.
     """
-    for file_name in os.listdir(folder_path):
-        if file_name.endswith('.csv'):
-            csv_file_path = os.path.join(folder_path, file_name)
-            df = pd.read_csv(csv_file_path)
-            if perform_individual_analysis:
-                prompt_name = os.path.basename(folder_path).split('_')[-1]
-                pdf_file_path = os.path.join(folder_path, f'prompt_{prompt_name}_analysis.pdf')
-                generate_pdf_report(df, pdf_file_path, prompt_name)
-                print(f'Generated PDF report for prompt {prompt_name} at {pdf_file_path}')
+    all_data = []
+    for root, dirs, _ in os.walk(base_path):
+        for dir_name in dirs:
+            if dir_name.startswith('prompt_'):
+                full_path = os.path.join(root, dir_name)
+                for file_name in os.listdir(full_path):
+                    if file_name.endswith('.csv'):
+                        csv_file_path = os.path.join(full_path, file_name)
+                        df = pd.read_csv(csv_file_path)
+                        if perform_individual_analysis:
+                            prompt_name = dir_name.split('_')[-1]
+                            pdf_file_path = os.path.join(full_path, f'prompt_{prompt_name}_analysis.pdf')
+                            generate_pdf_report(df, pdf_file_path, prompt_name)
+                            print(f'Generated PDF report for prompt {prompt_name} at {pdf_file_path}')
+                        df['Prompt'] = dir_name  # Adding a column to identify the prompt
+                        all_data.append(df)
+    return pd.concat(all_data, ignore_index=True)
 
 def run_analysis():
     """
     Run the analysis on the generated code files and produce reports.
     """
     base_folder_path = 'generated_code_files'
-    for root, dirs, _ in os.walk(base_folder_path):
-        for dir_name in dirs:
-            if dir_name.startswith('prompt_'):
-                full_path = os.path.join(root, dir_name)
-                process_csv_in_folder(full_path, True)
+    # Process and aggregate CSV files
+    global_data = process_and_aggregate_csv_files(base_folder_path, True)
+
+    # Generate global report
+    global_report_path = os.path.join(base_folder_path, 'global_analysis_report.pdf')
+    generate_global_pdf_report(global_data, global_report_path)
+    print(f'Generated global analysis report at {global_report_path}')
--- generate_global_report.py
+++ generate_global_report.py
@@ -1,3 +1,103 @@
-'''
-This script generates a global report after analyzing all the prompt analysis reports in the generated_code_files folder.
-'''
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer, PageBreak
+from reportlab.lib import colors
+from reportlab.lib.styles import getSampleStyleSheet
+
+def calculate_global_stats(df):
+    """
+    Calculate global statistics from the aggregated DataFrame.
+
+    :param df: DataFrame containing the test results from all prompts.
+    :return: A dictionary containing global statistics.
+    """
+    global_delta_analysis = df.groupby('Delta')['Pass/Fail'].value_counts().unstack(fill_value=0)
+    global_delta_analysis['Pass'] = global_delta_analysis.get('Pass', pd.Series(index=global_delta_analysis.index, data=[0]*len(global_delta_analysis)))
+    global_delta_analysis['Fail'] = global_delta_analysis.get('Fail', pd.Series(index=global_delta_analysis.index, data=[0]*len(global_delta_analysis)))
+
+    # Calculate pass ratio for each delta
+    global_delta_analysis['Pass Ratio'] = global_delta_analysis['Pass'] / (global_delta_analysis['Pass'] + global_delta_analysis['Fail'])
+
+    return global_delta_analysis
+
+def create_deltas_key_table(df):
+    """
+    Create a table for the Deltas Key section in the PDF document.
+
+    :param df: DataFrame containing the test results.
+    :return: Table object for the Deltas Key section.
+    """
+    deltas_components = df[['Delta', 'Components']].drop_duplicates().sort_values(by='Delta')
+    data = [['Delta', 'Components']] + deltas_components.values.tolist()
+
+    table = Table(data)
+    table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+    return table
+
+def calculate_global_stats(df):
+    """
+    Calculate global statistics from the aggregated DataFrame.
+
+    :param df: DataFrame containing the test results from all prompts.
+    :return: A dictionary containing global statistics.
+    """
+    global_delta_analysis = df.groupby('Delta')['Pass/Fail'].value_counts().unstack(fill_value=0)
+    global_delta_analysis['Pass'] = global_delta_analysis.get('Pass', pd.Series(index=global_delta_analysis.index, data=[0]*len(global_delta_analysis)))
+    global_delta_analysis['Fail'] = global_delta_analysis.get('Fail', pd.Series(index=global_delta_analysis.index, data=[0]*len(global_delta_analysis)))
+
+    # Calculate pass ratio for each delta
+    global_delta_analysis['Pass Ratio'] = global_delta_analysis['Pass'] / (global_delta_analysis['Pass'] + global_delta_analysis['Fail'])
+
+    return global_delta_analysis
+
+def generate_global_pdf_report(df, file_path):
+    """
+    Generate a global PDF report from the aggregated DataFrame.
+
+    :param df: DataFrame containing the aggregated test results from all prompts.
+    :param file_path: Path where the global PDF report will be saved.
+    """
+    doc = SimpleDocTemplate(file_path, pagesize=letter)
+    elements, styleSheet = [], getSampleStyleSheet()
+
+    elements.append(Paragraph("<b>Global Analysis Report</b>", styleSheet['Title']))
+    elements.append(Spacer(1, 12))
+
+    global_stats = calculate_global_stats(df)
+
+    # Create table for global statistics
+    global_stats_table_data = [["Delta", "Pass", "Fail", "Pass Ratio"]] + [
+        [delta, row['Pass'], row['Fail'], f"{row['Pass Ratio']:.2f}"] for delta, row in global_stats.iterrows()
+    ]
+
+    global_stats_table = Table(global_stats_table_data)
+    global_stats_table.setStyle(TableStyle([
+        ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+        ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+        ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+        ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+        ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+        ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+        ('GRID', (0, 0), (-1, -1), 1, colors.black)
+    ]))
+
+    elements.append(global_stats_table)
+
+    deltas_key_table = create_deltas_key_table(df)
+    elements.append(PageBreak())
+    elements.append(Paragraph("<b>Deltas Key</b>", styleSheet['Heading1']))
+    elements.append(deltas_key_table)
+
+    # Build the PDF
+    doc.build(elements)
