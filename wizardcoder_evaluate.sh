7b8ca168d2492079851909312a890262436aeda2
mg4607 <mg4607@columbia.edu>
2024-01-28 17:43:44 -0500

    run-2 in eval script

--- wizardcoder_evaluate.sh
+++ wizardcoder_evaluate.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#DATASET="humaneval mbpp"
-DATASET="humaneval"
-MODELS="wizard_7B"
-#MODELS="wizard_7B wizard_15B"
+DATASET="humaneval mbpp"
+#DATASET="humaneval"
+#MODELS="wizard_7B"
+MODELS="wizard_7B wizard_15B"
 NUM_DELTAS=7
 RESULT=''
 
@@ -17,13 +17,18 @@ do
         for ((i = 0; i < $NUM_DELTAS; i++)); do
             echo "Starting Delta: " $i
             EVAL=$(python3 evaluate_deltas.py --dataset $DATASET --samples generated_code/deltas/${DATA}_${MODEL}_run1_Delta_${i}.jsonl)
-            RESULT="$RESULT\n\n$DATA $MODEL Delta $i\nMemorization:$memorization\n$EVAL"
+            RESULT="$RESULT\n\n$DATA $MODEL Delta $i Run-1\nMemorization:$memorization\n$EVAL"
         done
-        python3 reports/process_wizardcoder.py --dataset humaneval --model wizard_7B --run-id 2
 
+        memorization=$(python3 reports/process_wizardcoder.py --dataset $DATA --model $MODEL --run-id 2 --num-deltas $NUM_DELTAS)
+        
+        echo "Evaluation: " $DATA " " $MODEL "\n"
         for ((i = 0; i < $NUM_DELTAS; i++)); do
-            evalplus.evaluate --dataset $DATASET --samples /generated_code/deltas${DATA}_${MODEL}_run2_Delta_${i}.jsonl
+            echo "Starting Delta: " $i
+            EVAL=$(python3 evaluate_deltas.py --dataset $DATASET --samples generated_code/deltas/${DATA}_${MODEL}_run2_Delta_${i}.jsonl)
+            RESULT="$RESULT\n\n$DATA $MODEL Delta $i Run-2\nMemorization:$memorization\n$EVAL"
         done
+
     done
 
 done
